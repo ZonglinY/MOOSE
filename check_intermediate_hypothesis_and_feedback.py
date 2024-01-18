a8215ef10bcf4cfae8276b64161afedb0e544127
Yang Zonglin Phd CIL <zonglin001@scsegc.cm.cluster>
2024-01-18 16:59:00 +0800

    can output 1. retrieved research background, 2.retrieved research inspiractions, 3. future-feedback (suggestions/explanations), 4. hypothesis, 5. present-feedback for hypothesis in the same time for check_intermediate_hypothesis_and_feedback.py

--- check_intermediate_hypothesis_and_feedback.py
+++ check_intermediate_hypothesis_and_feedback.py
@@ -20,13 +20,24 @@ def main():
 
     data = torch.load(os.path.join(args.checkpoint_dir, "background_inspiration_hypotheses.pt"))
     research_background = data[2][args.research_background_id]
+    research_inspirations = data[6][research_background][0]
     hypothesis = data[8][research_background][0][args.hypothesis_id][args.hypothesis_refinement_round]
     present_feedback = data[10][research_background][0][hypothesis]
+    future_feedback = data[15][research_background][0][0]
     
-    print("\n####### Hypothesis #######\n", hypothesis)
-    print("\n####### Present-feedback #######")
-    print("\n#### Reality Feedback ####\n{}\n\n#### Novelty Feedback ####\n{}\n\n#### Clarity Feedback ####\n{}\n\n".format(present_feedback[0], present_feedback[1], present_feedback[2]))
+    print("\n####### Research background #######\n", research_background)
 
+    print("\n####### Research inspirations #######")
+    for cur_insp_id, cur_insp in enumerate(research_inspirations):
+        print("Inspiration {}: \t{}".format(cur_insp_id, cur_insp))
+
+    print("\n####### Future-feedback (suggestions/explanations to be used for hypothesis generation) #######")
+    print(future_feedback)
+
+    print("\n####### Hypothesis #######\n", hypothesis)
+
+    print("\n####### Present-feedback #######")
+    print("#### Reality Feedback ####\n{}\n\n#### Novelty Feedback ####\n{}\n\n#### Clarity Feedback ####\n{}".format(present_feedback[0], present_feedback[1], present_feedback[2]))
 
 
 
